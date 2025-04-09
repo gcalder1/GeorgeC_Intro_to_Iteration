5610077a82e8eab43d7ee5e9492f8259aa2ef6ce
unknown <george.calderon2000@gmail.com>
2025-04-08 21:06:27 -0400

    Finished exercise 3
    Shared my exercise 3 as well. Seems like it could work. Its a static
    solution thought according to Jose.

--- script.js
+++ script.js
@@ -12,3 +12,38 @@ for(let person = 0; person < 5; person++){
     };
 };
 
+//+++Exercise 2+++//
+
+let triangleHash = 0;
+
+while (triangleHash < 7) {
+    console.log("#".repeat(triangleHash + 1)); //.repeat loops this but adds another of the triangle hash to it
+    triangleHash++; //increases till the next loop until it reaches anything less than 7
+    
+};
+
+console.log("finished!")
+
+//+++Exercise 3++//
+
+let doubleUp = 2;
+
+console.log("Welcome to Double Up! Input a number that is not less than 1")
+
+if (doubleUp >= 1) {
+do {
+    doubleUp = doubleUp * 2;
+    console.log(`Current: ${doubleUp}`)
+
+} while (doubleUp <= 100) {
+    console.log(`Uh oh, ${doubleUp} is greater than 100`);
+};
+
+console.log("Game Over!");
+}
+
+else {
+    console.log("Are you trying to break me?")
+}
+
+//+++Exercise 4+++//
