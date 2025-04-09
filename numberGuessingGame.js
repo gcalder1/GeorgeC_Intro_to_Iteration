7fd51589e966eb127f4d5103f9eca671115e8240
unknown <george.calderon2000@gmail.com>
2025-04-09 01:34:08 -0400

    Finished adding the remaining code for the game
    I added the necessary alerts for the if statements to prompt based on the choice.
    I also added console logs to make sure I was getting the right things.
    And finally I added the correct condition to check for with the while loop.
    The reason this works is because the user will keep getting prompted no matter
    how many times they get it wrong since the user value is still not equal to the
    generated number. Only when the number guess is exactly equal to the random
    number that was generated will we receive the victory alert.

---
+++ numberGuessingGame.js
@@ -0,0 +1,19 @@
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
+let userGuess = null;
+
+while (userGuess !== randomNumber) {
+    userGuess = parseInt(prompt("Guess a number between 1 and 100"));
+    console.log(userGuess);
+
+    if (userGuess < randomNumber) {
+        alert("Too low! Try again.")
+    }
+
+    if (userGuess > randomNumber) {
+        alert("Too high! Try again.")
+    }
+        
+}
+
+alert("Congratulations! You guessed the correct number.");
